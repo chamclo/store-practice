2fdd9f55176eaa4cff3e4b06186a4ee3bf137776
Cloey <chamclo@gmail.com>
2018-02-27 10:49:54 -0500

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  get 'view_order'=> 'cart#view_order'
+
+  get 'checkout' => 'cart#checkout'
+
+  # devise_for :users
+
+  root 'storefront#all_items'
+
+  get 'categoric' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
+
+
+
+
+  devise_for :users
+  resources :products
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
